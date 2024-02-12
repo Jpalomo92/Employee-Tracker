9c17c2012eec29a51c72a667e88a7ee6be3e2a3f
Jpalomo <Jpalomo92@gmail.com>
2024-02-11 20:38:11 -0600

    added foreign keys to roles and employees tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,28 +5,33 @@ USE registrar_db;
 
 CREATE TABLE department (
 
-  id INT NOT NULL,
-  department VARCHAR(30) NOT NULL
-
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department VARCHAR(30) NOT NULL,
+  
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
 
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  department_id INT NOT NULL
-
+  department_id INT
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
 
 );
 
 CREATE TABLE employee (
 
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT
+  role_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES  roles(id)
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 
 );
 
