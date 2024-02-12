ccdbb00412b4c6c677dc6315bfbe2f5d5741acbe
Jpalomo <Jpalomo92@gmail.com>
2024-02-11 19:55:27 -0600

    added three tables(department, role, and employee) to the schema file

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS registrar_db;
+CREATE DATABASE registrar_db;
+
+USE registrar_db;
+
+CREATE TABLE department (
+
+  id INT NOT NULL,
+  department VARCHAR(30) NOT NULL
+
+);
+
+CREATE TABLE role (
+
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT NOT NULL
+
+
+);
+
+CREATE TABLE employee (
+
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+
+);
+
