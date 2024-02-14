4bbd3cff30e16313176cebb148dddd1771f61dec
Jpalomo <Jpalomo92@gmail.com>
2024-02-14 01:16:48 -0600

    added prompt and functions for departments roles and employees

--- index.js
+++ index.js
@@ -19,10 +19,101 @@ const db = mysql.createConnection(
     database: 'registrar_db'
   },
   console.log(`Connected to the registrar_db database.`)
-);
+)
 
+db.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  mainPrompt();
+});
 
+function mainPrompt(){
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'userChoice',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
 
+    }
+  ]).then((res) =>{
+    console.log(res.userChoice);
+    switch(res.userChoice){
+      case 'View all departments':
+       viewAllDepartments();
+       break;
+
+      case 'View all roles':
+       viewAllRoles();
+       break;
+
+      case 'View all employees':
+       viewAllEmployees();
+       break;
+
+      case 'Add a department':
+      addDepartment();
+       break;
+
+      case 'Add a role':
+       addRole();
+       break;
+
+      case 'Add an employee':
+       addEmployee();
+       break;
+
+      case 'Update an employee role':
+       updateEmployeeRole();
+       break;
+
+      case 'Exit':
+       db.end();
+       break;
+    }
+  }).catch((err)=>{
+    if(err) throw err;
+  })
+}
+
+//see all department names and ids
+function viewAllDepartments(){
+  let query = 'SELECT department.id, department.name AS department FROM department'
+  db.query(query, (err, res) =>{
+    if (err) throw err;
+    console.table(res);
+    mainPrompt();
+  })
+}
+
+//see roles id, title, department, and salary
+function viewAllRoles(){
+  let query = 'SELECT roles.id, roles.title, roles.salary, department.name AS department FROM roles JOIN department ON department.id = roles.department_id'
+  db.query(query, (err, res) =>{
+    if (err) throw err;
+    console.table(res);
+    mainPrompt();
+  })
+}
+
+//see all employee id, first and last name, job title, department, salary, and manager
+function viewAllEmployees(){
+  let query = 'SELECT employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, department.name AS department, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee JOIN roles ON roles.id = employee.role_id JOIN department ON department.id = roles.department_id JOIN employee manager ON manager.id = employee.manager_id'
+  db.query(query, (err, res) =>{
+    if (err) throw err;
+    console.table(res);
+    mainPrompt();
+  })
+}
 
 // db.query('SELECT * FROM department', function (err, results) {
 //     console.log(results);
